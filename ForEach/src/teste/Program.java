035bcd42998dd59e9089d02a54f84280c12b0467
Gabriel <gabriel.brandao@ccc.ufcg.edu.br>
2019-04-18 22:55:03 -0300

    For Each em java

---
+++ ForEach/src/teste/Program.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] nomes = {"joao", "maria", "pedro"};
+		for (String obj : nomes) {
+			System.out.println(obj);
+		}		
+	}
+
+}
