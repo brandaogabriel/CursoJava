6e787df3e8b4a722f0cbce137482653c84b04ec5
Gabriel <gabriel.brandao@ccc.ufcg.edu.br>
2019-04-28 17:10:13 -0300

    excecoes

---
+++ Execoes/src/application/Trycatch.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Trycatch {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException aib) {
+			System.out.println("Invalid position!");
+		}
+		catch (InputMismatchException ime) {
+			System.out.println("Input error!");
+		}
+		
+		System.out.println("End of program");
+		
+		sc.close();
+	}
+
+}
